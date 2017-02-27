a5c3e59109b9cfdfe91943671183c65a7280dcd0
Eric Purvis <purvis.38@osu.edu>
2017-02-27 12:20:41 -0500

    added basic firebase post

---
+++ app/src/main/java/com/example/ericrpurvis/xplorer/LocationPost.java
@@ -0,0 +1,22 @@
+package com.example.ericrpurvis.xplorer;
+
+/**
+ * Created by ericrpurvis on 2/21/17.
+ */
+
+public class LocationPost {
+
+    public String name;
+    public String desc;
+    public String actType;
+
+    public LocationPost(){
+
+    }
+
+    public LocationPost(String name, String desc, String actType){
+        this.name = name;
+        this.desc = desc;
+        this.actType = actType;
+    }
+}
