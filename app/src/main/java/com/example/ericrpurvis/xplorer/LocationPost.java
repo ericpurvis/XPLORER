ae5f5ae3981dab093feb1b21eaee60634a60c98c
pbmuller <muller.98@osu.edu>
2017-02-27 12:46:19 -0500

    Working on adding MapsActivity and activity_maps

--- app/src/main/java/com/example/ericrpurvis/xplorer/LocationPost.java
+++ app/src/main/java/com/example/ericrpurvis/xplorer/LocationPost.java
@@ -1,5 +1,7 @@
 package com.example.ericrpurvis.xplorer;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by ericrpurvis on 2/21/17.
  */
@@ -9,14 +11,16 @@ public class LocationPost {
     public String name;
     public String desc;
     public String actType;
+    public LatLng coord;
 
     public LocationPost(){
 
     }
 
-    public LocationPost(String name, String desc, String actType){
+    public LocationPost(String name, String desc, String actType, LatLng coord){
         this.name = name;
         this.desc = desc;
         this.actType = actType;
+        this.coord = coord;
     }
 }
