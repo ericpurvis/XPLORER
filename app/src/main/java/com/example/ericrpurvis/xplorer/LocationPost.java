ec1131894cde41821ba19898c3d2d640f275f813
Eric Purvis <purvis.38@osu.edu>
2017-03-06 12:49:56 -0500

    added post locaitons

--- app/src/main/java/com/example/ericrpurvis/xplorer/LocationPost.java
+++ app/src/main/java/com/example/ericrpurvis/xplorer/LocationPost.java
@@ -1,6 +1,7 @@
 package com.example.ericrpurvis.xplorer;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by ericrpurvis on 2/21/17.
@@ -11,16 +12,20 @@ public class LocationPost {
     public String name;
     public String desc;
     public String actType;
-    public LatLng coord;
+    public FirebaseUser user;
+    public double lat;
+    public double lng;
 
     public LocationPost(){
 
     }
 
-    public LocationPost(String name, String desc, String actType, LatLng coord){
+    public LocationPost(String name, String desc, String actType,double lat,double lng, FirebaseUser user){
         this.name = name;
         this.desc = desc;
         this.actType = actType;
-        this.coord = coord;
+        this.user = user;
+        this.lat = lat;
+        this.lng = lng;
     }
 }
--- app/src/main/java/com/example/ericrpurvis/xplorer/PostActivity.java
+++ app/src/main/java/com/example/ericrpurvis/xplorer/PostActivity.java
@@ -1,68 +1,154 @@
 package com.example.ericrpurvis.xplorer;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.view.View.OnClickListener;
+import android.widget.Spinner;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class PostActivity extends AppCompatActivity implements View.OnClickListener {
+public class PostActivity extends AppCompatActivity implements OnClickListener, OnItemSelectedListener, LocationListener {
 
     private DatabaseReference databaseReference;
-    private EditText editTextName, editTextDesc, editTextActType;
+    private EditText editTextName, editTextDesc;
+    private Spinner editActType;
     private Button buttonPost;
+
+    private LocationManager locationManager;
+    private String provider;
+
 
     private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_location);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference();
 
+        //db init
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Locations");
+
+        //edit text/spinner init
         editTextName = (EditText) findViewById(R.id.name);
+
+        editActType = (Spinner) findViewById(R.id.actType);
+        editActType.setOnItemSelectedListener(this);
+
         editTextDesc = (EditText) findViewById(R.id.description);
-        editTextActType = (EditText) findViewById(R.id.activityType);
 
         buttonPost = (Button) findViewById(R.id.buttonPost);
-
         buttonPost.setOnClickListener(this);
 
         //initializing firebase auth object
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //location init
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 3000, 1,this);
+
+
     }
 
 
-    private void getInformation(){
-        String name = editTextName.getText().toString().trim();
-        String desc = editTextDesc.getText().toString().trim();
-        String actType = editTextActType.getText().toString().trim();
+    private void getInformation() {
 
-        LocationPost location = new LocationPost(name, desc, actType);
+        String name = editTextName.getText().toString().trim();
+        String actType = editActType.getSelectedItem().toString().trim();
+        String desc = editTextDesc.getText().toString().trim();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        Location loc = locationManager.getLastKnownLocation(provider);
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        databaseReference.child(user.getUid()).setValue(location);
+        LocationPost locationPost = new LocationPost(name, desc, actType,loc.getLatitude(),loc.getLongitude(),user);
 
-        Toast.makeText(this,"Location Saved", Toast.LENGTH_LONG).show();
+        String key = databaseReference.push().getKey();
+
+        databaseReference.child(key).setValue(locationPost);
+
+        Toast.makeText(this, "Location Saved", Toast.LENGTH_LONG).show();
 
     }
 
     @Override
     public void onClick(View view) {
 
-        if(view == buttonPost){
+        if (view == buttonPost) {
             getInformation();
         }
+    }
 
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(PostActivity.this,parent.getSelectedItem().toString()+" Selected",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(i);
     }
 }
